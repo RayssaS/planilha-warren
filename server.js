2d509884026ce05181bdb91a7ce699dbb1ae4d7e
RayssaS <rayssasouzaunb@gmail.com>
2022-08-18 16:22:37 -0300

    adiciona-servidor

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs').promises
+
+const hostname = 'localhost'
+const port = 3000;
+
+const server = htttp.createServer(trataReq);
+
+    function trataReq (req, res){
+        fs.readFile('./index.html', 'utf8').then(texto => {
+            res.statusCode = 200;
+            res.setHeader("Content-type", text/html);
+            res.write(texto);
+            res.end;
+        })
+    }
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`)
+    })
